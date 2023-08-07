e49dc6046bc471218714b329f129e56123c123a0
Yevhen <mr.undesired@gmail.com>
2023-08-07 20:08:47 +0300

    import style CSS

--- src/Content.tsx
+++ src/Content.tsx
@@ -1,21 +1,16 @@
+import './Content.css'
+
 interface Props {
     text: string
     text2: string
     year: number
 }
+
 export const Content = (props: Props) => {
     return (
         <>
             <div>
-                <p
-                    style={{
-                        color: 'white',
-                        backgroundColor: 'green',
-                        padding: '15px',
-                    }}
-                >
-                    {props.text}
-                </p>
+                <p className="content">{props.text}</p>
                 <p>{props.text2}</p>
                 <p>{props.year}</p>
             </div>
