6ef35e66573e5f0908c5aac31e7cc25dc4e17e20
Yevhen <mr.undesired@gmail.com>
2023-08-09 21:16:51 +0300

    add materials and header

--- src/container/App/App.tsx
+++ src/container/App/App.tsx
@@ -1,11 +1,13 @@
 import Header from 'container/Header/Header'
 import Main from 'container/Main/Main'
 import Footer from 'container/Footer/Footer'
+import CssBaseline from '@mui/material/CssBaseline'
 
 interface Props {}
 const App = (props: Props) => {
     return (
         <>
+            <CssBaseline />
             <Header />
             <Main />
             <Footer />
