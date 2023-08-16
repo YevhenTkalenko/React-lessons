6b87e7d16eb65135d146f2c9ae0b1a0bdb4abce6
Yevhen <mr.undesired@gmail.com>
2023-08-16 20:33:32 +0300

    add card description

--- src/components/ProductList/ProductList.tsx
+++ src/components/ProductList/ProductList.tsx
@@ -1,7 +1,6 @@
 import { Grid, Typography } from '@mui/material'
 import ProductsListItem from './ProductsListItem'
 
-
 interface Props {}
 const ProductList = (props: Props) => {
     return (
@@ -12,16 +11,31 @@ const ProductList = (props: Props) => {
 
             <Grid container spacing={3}>
                 <Grid item xs={12} sm={6} md={4}>
-                    <ProductsListItem />
+                    <ProductsListItem
+                        title="Iphone 14 pro"
+                        description="iPhone 14 pro"
+                        type="phone"
+                        capacity="250 GB"
+                        price={1200}
+                    />
                 </Grid>
                 <Grid item xs={12} sm={6} md={4}>
-                    <ProductsListItem />
+                    <ProductsListItem
+                        title="Iphone 13 pro"
+                        description="iPhone 13 pro"
+                        type="phone"
+                        capacity="200 GB"
+                        price={1000}
+                    />
                 </Grid>
                 <Grid item xs={12} sm={6} md={4}>
-                    <ProductsListItem />
-                </Grid>
-                <Grid item xs={12} sm={6} md={4}>
-                    <ProductsListItem />
+                    <ProductsListItem
+                        title="Iphone 12 pro"
+                        description="iPhone 12 pro"
+                        type="phone"
+                        capacity="150 GB"
+                        price={800}
+                    />
                 </Grid>
             </Grid>
         </>
--- src/components/ProductList/ProductsListItem.tsx
+++ src/components/ProductList/ProductsListItem.tsx
@@ -1,16 +1,24 @@
 import { Button, Card, CardActions, CardContent } from '@mui/material'
 import './ProductsListItem.scss'
 
-interface Props {}
+interface Props {
+    title: string
+    description: string
+    type: string
+    capacity: string
+    price: number
+}
 const ProductsListItem = (props: Props) => {
     return (
         <Card variant="outlined" className="product">
             <CardContent>
-                <div className="product-title">iPhone 14 pro</div>
-                <div className="product-descr">This is iPhone 14 pro</div>
-                <div className="product-features">Type: phone</div>
-                <div className="product-features">Capacity: 250 GB</div>
-                <div className="product-price">1200$</div>
+                <div className="product-title">{props.title}</div>
+                <div className="product-descr">This is {props.description}</div>
+                <div className="product-features">Type: {props.type}</div>
+                <div className="product-features">
+                    Capacity: {props.capacity}
+                </div>
+                <div className="product-price">{props.price}$</div>
             </CardContent>
             <CardActions className="product-btn-wrap">
                 <Button variant="outlined">Add to cart</Button>
